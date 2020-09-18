aa1185498fe536432ce523a33aa78de5eccdf808
[suraj] <[suraj1996xyz@gmail.com]>
2020-09-18 15:13:27 +0530

    Updates

---
+++ precache-manifest.e141cf5a8deeff5b9fdcc29734d1c7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ff58596a4d6c2f8147fbec03bc7c1e",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "671d5f8fa4fd731eb30a",
+    "url": "/website/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "45f7d7b627e4d00021af",
+    "url": "/website/static/js/2.0ae06082.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.0ae06082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671d5f8fa4fd731eb30a",
+    "url": "/website/static/js/main.b215cd0f.chunk.js"
+  },
+  {
+    "revision": "c257f4c7d387cdd8a1cc",
+    "url": "/website/static/js/runtime-main.2a443dc8.js"
+  }
+]);
